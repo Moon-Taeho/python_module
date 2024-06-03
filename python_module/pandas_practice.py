a51af1c3d906f7f317b2040c843a0a6bad489872
MoonTaeho <moon.taeho@mic-p.com>
2024-06-03 11:20:08 +0900

    git ignore

--- python_module/pandas_practice.py
+++ python_module/pandas_practice.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
+# series data
 """ series_data = [10, 15, 20, 25]
 ser = pd.Series(series_data)
 
 print(ser) """
 
-dataframe_data = ([
+# dataframe data
+""" dataframe_data = ([
     [100, "a", True],
     [200, "b", False],
     [150, "d", False],
@@ -17,4 +19,6 @@ df = pd.DataFrame(dataframe_data)
 df.index = ["01", "02", "03", "04"]
 df.columns = ["A", "B", "C"]
 
-print(df[["A", "B"]])
+print(df[["A", "B"]]) """
+
+df = pd.read_csv("./titanic.csv")
